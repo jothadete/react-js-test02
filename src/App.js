6328ab705bb8fb0350c65fee7bd3d337ce2996d7
jothadete <jens.tenter@live.com>
2022-05-12 14:51:34 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ export default function App() {
       <p>Im Terminal npm install, um die Bibliotheken in das Verzeichnis zu laden. Dies git-ignored.</p>
       <p>npm start - funktioniert soweit.</p>
       <p>commit changes - push to GitHub. Ende in VSCode</p>
-      
+      <h2>StackBlitz 2</h2>
+      <p>Im geöffneten Projekt die Aktualisierungspfeile neben dem Repo geklickt. Projekt auf dem Status des Repo.</p>
+      <p>Neuen Branch angelegt (Name: stackBlitz). Die Beschreibung hier ergänzt (aber "StackBlitz 2").</p>
     </div>
   );
 }

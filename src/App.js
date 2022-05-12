5c0a6baa9e32ce01e2b0291200aedc7d5f855cea
Jens Tenter <jens.tenter@live.com>
2022-05-12 14:47:26 +0200

    vscode projekt angelegt

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <h2>StackBlitz</h2>
       <p>Auf Stackblitz neu angelegt. TypeScript funktionierte nicht.</p>
-      <p>Neues Repository in Github angelegt und gepushed. Ende in StackBlitz</p>
+      <p>Neues Repository in Github angelegt und gepushed. Ende in StackBlitz.</p>
+      <h2>VSCode</h2>
+      <p>Geclont von GitHub, dabei neuen leeren Folder angelegt.</p>
+      <p>Im Terminal npm install, um die Bibliotheken in das Verzeichnis zu laden. Dies git-ignored.</p>
+      <p>npm start - funktioniert soweit.</p>
+      <p>commit changes - push to GitHub. Ende in VSCode</p>
+      
     </div>
   );
 }

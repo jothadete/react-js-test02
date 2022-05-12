1934099c957bc097285600373c77a91aa340a6ac
jothadete <jens.tenter@live.com>
2022-05-12 14:29:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-7wcba6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-7wcba6)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <p>Auf Stackblitz neu angelegt. TypeScript funktionierte nicht.</p>
+      <p>Neues Repository in Github angelegt und gepushed. Ende in StackBlitz</p>
+    </div>
+  );
+}
